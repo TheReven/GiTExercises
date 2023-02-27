16e7b29526bf3fbbfda98a5afd3da298ba9b6679
TheReven <reven20002@gmail.com>
2023-02-27 19:28:27 +0000

    Update Exercise11.py

--- Exercise11/Exercise11.py
+++ Exercise11/Exercise11.py
@@ -1,22 +1,23 @@
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
 
-# a)
-Belgium_whole_hyphens = '-' * len(Belgium.split(','))
+# Task - A)
+
+Belgium_whole_hyphens = "-" * len(Belgium.split(","))
 print(Belgium_whole_hyphens)
-Belgium_hyphens = '-' * len(Belgium.split(',')[0])
-print(Belgium_hyphens)
-Brussels_hyphens = '-' * len(Belgium.split(',')[2])
-print(Brussels_hyphens)
+Belgium_hyphens = "-" * len(Belgium.split(',')[0])
+print("Belgium in hyphens: ", Belgium_hyphens)
+Brussels_hyphens = "-" * len(Belgium.split(',')[2])
+print("Brussels in hyphens: ",  Brussels_hyphens)
 
-# b)
+# Task - B)
 
-colons = Belgium.replace(',', ':')
+colons = Belgium.replace(",", ":")
 print("String with Colons: " + colons)
-semicolons = Belgium.replace(',', ';')
+semicolons = Belgium.replace(",", ";")
 print("String with Semi Colons: " + semicolons)
 
-# c)
+# Task - C)
 
-cities = Belgium.split(',')
+cities = Belgium.split(",")
 population = int(cities[1]) + int(cities[3])
 print("Belgium + Brussels Population:", + population)
