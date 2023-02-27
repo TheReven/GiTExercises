20e06d109dd9194df3b65683bea93c1d590eab69
TheReven <reven20002@gmail.com>
2023-02-27 19:41:17 +0000

    Update Exercise11.py

--- Exercise11/Exercise11.py
+++ Exercise11/Exercise11.py
@@ -5,7 +5,7 @@ Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,F
 Belgium_whole_hyphens = "-" * len(Belgium.split(","))
 print(Belgium_whole_hyphens)
 Belgium_hyphens = "-" * len(Belgium.split(',')[0])
-print("Belgium in hyphens: ", Belgium_hyphens)
+print("Belgium in hyphens: ",  Belgium_hyphens)
 Brussels_hyphens = "-" * len(Belgium.split(',')[2])
 print("Brussels in hyphens: ",  Brussels_hyphens)
 
@@ -20,4 +20,4 @@ print("String with Semi Colons: " + semicolons)
 
 cities = Belgium.split(",")
 population = int(cities[1]) + int(cities[3])
-print("Belgium + Brussels Population:", + population)
+print(f"Belgium + Brussels Population: {population}")
